cbb497583d02c97ad4f6c5ce2334a9f7ca65055c
Gionni <giovbat@gmail.com>
2016-08-23 22:48:04 +0200

    autokill daemon

---
+++ heathledger.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Born as a tool this script will be a daemon in the future.
+# It will help you, how?
+# Well... it checks if some zombie jocker is around and kill himself
+# with the Jocker.
+
+
+echo "a lot of fun behind the scene"
